224b6b3c2bc3d36a03dc8331ad8c14faad2b641c
Roninhij <ronin1hij@hotmail.com>
2024-04-14 23:22:15 +0300

    finished more exercises

---
+++ JavaScript/Primitive-Data-Types/6-typeof.js
@@ -0,0 +1,36 @@
+let num = 10;
+let str = "hello";
+let isbool = true;
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof isbool);
+
+let numb = 42;
+let numb2 = 42;
+console.log(typeof numb2);
+console.log(typeof numb);
+
+const bathroom = undefined;
+const emptyValue = null;
+console.log(typeof bathroom);
+console.log(typeof emptyValue);
+
+// let number4 = 12;
+// let na = "Money";
+// let x = 15;
+// x =
+//   " The Value of " +
+//   x +
+//   " is 10 and its type is  " +
+//   number4 +
+//   console.log(typeof number4);
+// console.log(typeof na);
+// console.log(typeof boolean);
+
+let x = 10;
+let y = "Hello";
+let z = true;
+
+console.log("The value of x is " + x + " and its type is " + typeof x);
+console.log("The value of y is " + y + " and its type is " + typeof y);
+console.log("The value of z is " + z + " and its type is " + typeof z);
---
+++ JavaScript/Primitive-Data-Types/7-Concatenation.js
@@ -0,0 +1,35 @@
+// const a = " My name is Omar " + " i 22 am years old ";
+// // const s = 22;
+// // a = " My name is " + a + " and i am " + s + " years old ";
+// console.log(a);
+// // console.log(s);
+
+// const myName = "Omar";
+// const myAge = 22;
+
+// console.log("My name is " + myName + " and I am " + myAge + " years old.");
+
+const namee = "Omar";
+const myAgee = 22;
+console.log("My name is " + namee + " and I am " + myAgee + " years old.");
+
+const fod = "Pizza";
+const colr = "grey";
+const watch = "godzilla";
+
+console.log(
+  " My favorite color is " +
+    colr +
+    " I love the movie " +
+    watch +
+    " I also enjoy Eating " +
+    fod
+);
+
+const a = 10;
+const b = 5;
+const numbers = a + b;
+console.log("The sum of " + a + " and " + b + " is " + numbers);
+
+const day = "friday";
+console.log("Today is " + day);
